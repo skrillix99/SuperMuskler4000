736082a24d68ba6f2a59d16c057a9ddea310076d
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2022-11-30 11:56:23 +0100

    Tilføjet Axios, Fået API ninjas til at vise data baseret på hvilken muskelgruppe man vælger

--- SuperMuskler4000/js/index.js
+++ SuperMuskler4000/js/index.js
@@ -1,8 +1,22 @@
 console.log("Test")
+const baseURL = "https://api.api-ninjas.com/v1/exercises"
+
+
+
 Vue.createApp({
     data() {
         return {
-
+            name: "",
+            type: "",
+            muscle: "",
+            equipment: "",
+            difficulty: "",
+            instructions: "",
+            exercise_list: [],
+            statuscode: null,
+            search_muscle: "Choose your Muscle Group"
+            
+            
         }
     },
 
@@ -11,6 +25,14 @@ Vue.createApp({
     },
 
     methods: {
-
+        GetAllExercises(search_muscle) {
+            uri = baseURL + "?muscle=" + search_muscle
+            axios.get(uri, {headers: {'X-Api-Key': 'BrKZG9qvMdQ2c8s1vUce8Q==O3S6L9vlhJ2wNmrJ'}})
+            .then(response => {
+                
+                this.exercise_list = response.data
+                
+            })
+        }
     }
 }).mount("#app")
