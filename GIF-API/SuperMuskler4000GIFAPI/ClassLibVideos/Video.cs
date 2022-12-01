16e6f317445e14358fcae6b21da124aa9875976d
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-12-01 14:28:33 +0100

    Added Tests to Video

--- GIF-API/SuperMuskler4000GIFAPI/ClassLibVideos/Video.cs
+++ GIF-API/SuperMuskler4000GIFAPI/ClassLibVideos/Video.cs
@@ -3,5 +3,31 @@
     public class Video
     {
 
+
+        #region Constructor
+
+        public Video() : this(1, "Barbell Curl", "https://i.imgur.com/adDxqu3.mp4")
+        {
+            
+        }
+
+        public Video(int id, string name, string videolink)
+        {
+            Id = id;
+            Name = name;
+            VideoLink = videolink;
+        }
+
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VideoLink { get; set; }
+
+
+        #endregion
     }
 }
---
+++ GIF-API/SuperMuskler4000GIFAPI/SuperMuskler4000APITest/VideoTests.cs
@@ -0,0 +1,60 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SuperMuskler4000APITest
+{
+    [TestClass]
+    public class VideoTests
+    {
+        private Video video;
+        
+        [TestInitialize]
+        public void TestInit()
+        {
+            video = new Video();
+        }
+        
+        
+        [TestMethod]
+        public void VideoLinkReturnsCorrectLink()
+        {
+            //Arrange
+            string expectedLink = "https://i.imgur.com/adDxqu3.mp4";
+            
+            //Act
+            video.VideoLink = "https://i.imgur.com/adDxqu3.mp4";
+            string actualLink = video.VideoLink;
+
+            //Assert
+            Assert.AreEqual(expectedLink, actualLink);
+        }
+
+        [TestMethod]
+        public void NameReturnsCorrectName()
+        {
+            //Arrange
+            string expectedName = "Barbell Curl";
+
+            //Act
+            video.Name = "Barbell Curl";
+            string actualName = video.Name;
+
+            //Assert
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [TestMethod]
+        public void IdReturnsCorrectId()
+        {
+            //Arrange
+            int expectedId = 1;
+
+            //Act
+            video.Id = 1;
+            int actualId = video.Id;
+
+            //Assert
+            Assert.AreEqual(expectedId, actualId);
+        }
+
+
+    }
+}
