7e8808038bbb3d4dff3100ea2b6efb0507497925
Dennis Møllegård Hansen <denn108g@edu.zealand.dk>
2022-12-08 11:23:23 +0100

    Added comments to Video and VideoManager Class

--- GIF-API/SuperMuskler4000GIFAPI/ClassLibVideos/Video.cs
+++ GIF-API/SuperMuskler4000GIFAPI/ClassLibVideos/Video.cs
@@ -1,16 +1,28 @@
 ﻿namespace ClassLibVideos
 {
+    /// <summary>
+    /// This is the Model Class for Videos
+    /// </summary>
     public class Video
     {
 
 
         #region Constructor
-
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Video() : this(1, "Barbell Curl", "https://i.imgur.com/adDxqu3.mp4","biceps")
         {
             
         }
 
+        /// <summary>
+        /// Constructor with 4 parameters
+        /// </summary>
+        /// <param name="id">The videos Id. Type int</param>
+        /// <param name="name">The videos name. Type string</param>
+        /// <param name="videolink">The link to the video. Type string</param>
+        /// <param name="muscleType">Muscletype of the exercise shown in the video. Type string</param>
         public Video(int id, string name, string videolink, string muscleType)
         {
             Id = id;
@@ -23,10 +35,21 @@
         #endregion
 
         #region Properties
-
+        /// <summary>
+        /// The Id property. Type string. Contains both get/set functionality
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// The name property. Type string. Contains both get/set functionality
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The video link property. Type string. Contains both get/set functionality
+        /// </summary>
         public string VideoLink { get; set; }
+        /// <summary>
+        /// The muscletype property. Type string. Contains both get/set functionality
+        /// </summary>
         public string MuscleType { get; set; }
 
 
--- GIF-API/SuperMuskler4000GIFAPI/SuperMuskler4000GIFAPI/Managers/VideoManager.cs
+++ GIF-API/SuperMuskler4000GIFAPI/SuperMuskler4000GIFAPI/Managers/VideoManager.cs
@@ -6,30 +6,44 @@ namespace SuperMuskler4000GIFAPI.Managers
 {
     public class VideoManager
     {
+        // stores the connectionstring to the database supermuskler4000 in the variable connectionString.
         string connectionString = "Data Source=supermuskler4000server.database.windows.net;Initial Catalog=SuperMuskler4000;User ID=admin1;Password=Alexander1;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         
+        /// <summary>
+        /// creates a list with videos where it get the data from the database and sortes them after mucletype, and if mucle type is null or empthy it throw an exception.
+        /// </summary>
+        /// <param name="muscletype"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public List<Video> GetByMuscleType(string muscletype)
         {
+            // throws an exception if mucle type is null or empthy.
             if (muscletype.IsNullOrEmpty())
             {
                 throw new ArgumentNullException("muscletype", "You must enter a muscletype");
             }
+            // create the list of vidoes
             List<Video> videoList = new List<Video>();
+            // creating the query, where it select mucletypes from supermuskler4000 database
             string queryString = "select * from Supermusklervideoer where MuscleType = @muscletype";
 
+            // establish connection to database using the connectionstring.
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                // creating the command we send to the database.
                 SqlCommand command = new SqlCommand(queryString, connection);
                 command.Connection.Open();
                 command.Parameters.AddWithValue("@muscletype", muscletype.ToLower());
-
+                // crates the reading founktion for the database
                 SqlDataReader reader = command.ExecuteReader();
-
+                // here we read the data from the database and puts it in the video list
                 while (reader.Read())
                 {
                     Video v = ReadVideo(reader);
                     videoList.Add(v);
                 }
+            // tjeks if the video list is empthy
             }
             if (videoList.Count == 0)
             {
@@ -37,6 +51,11 @@ namespace SuperMuskler4000GIFAPI.Managers
             }
             return videoList;
         }
+        /// <summary>
+        /// Reads one row of data from the database and puts the data into a Video object
+        /// </summary>
+        /// <param name="reader">Type SqlDataReader. Reads the data from the database</param>
+        /// <returns>Returns an object of the Video class</returns>
         private Video ReadVideo(SqlDataReader reader)
         {
             Video v = new Video();
